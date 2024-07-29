ea6a6bb280d410cbce1d01eb764b4be04757b57c
VitoriaSouzaTec <Vitoriasouzatec@gmail.com>
2024-07-29 20:02:05 -0300

    Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -1,64 +1,90 @@
 import React from 'react';
 import './Footer.css';
+import PropTypes from 'prop-types';
 
-
-function Footer() {
-
-  let logoFooter = "https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/logo-footer.svg"
-  let Facebook = "https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/facebook.svg"
-  let Instagram = "https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/instagram.svg"
-  let twitter = "https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/twitter.svg"
-
+function Footer({
+  logoFooter,
+  facebook,
+  instagram,
+  twitter,
+  infoLinks,
+  categorias,
+  endereco,
+  telefone,
+  copyAno,
+  marca
+ 
+}) {
   return (
     <footer className="footer">
       <div className="container">
         <div className="footer-section">
-          <img className='logo' src={logoFooter} alt='logo-digital college'/>
+          <img className="logo" src={logoFooter} alt="logo" />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
           <div className="social-media">
-            <a href="#">
-              <img src = {Facebook} alt="Facebook" />
+            {facebook && (
+              <a href={facebook}>
+                <img src={facebook} alt="Facebook" />
               </a>
-            <a href="#">
-              <img src = {Instagram} alt='instagram'/>
+            )}
+            {instagram && (
+              <a href={instagram}>
+                <img src={instagram} alt="Instagram" />
               </a>
-            <a href="#">
-              <img src ={twitter} alt='twitter'/>
+            )}
+            {twitter && (
+              <a href={twitter}>
+                <img src={twitter} alt="Twitter" />
               </a>
+            )}
           </div>
         </div>
         <div className="footer-section">
           <h3>Informação</h3>
           <ul>
-            <li><a href="#">Sobre Drip Store</a></li>
-            <li><a href="#">Segurança</a></li>
-            <li><a href="#">Wishlist</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Trabalhe conosco</a></li>
-            <li><a href="#">Meus Pedidos</a></li>
+            {infoLinks.map((link, index) => (
+              <li key={index}><a href={link.href}>{link.text}</a></li>
+            ))}
           </ul>
         </div>
         <div className="footer-section">
           <h3>Categorias</h3>
           <ul>
-            <li><a href="#">Camisetas</a></li>
-            <li><a href="#">Calças</a></li>
-            <li><a href="#">Bonés</a></li>
-            <li><a href="#">Headphones</a></li>
-            <li><a href="#">Tênis</a></li>
+            {categorias.map((category, index) => (
+              <li key={index}><a href={category.href}>{category.text}</a></li>
+            ))}
           </ul>
         </div>
         <div className="footer-section">
           <h3>Contato</h3>
-          <p>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
-          <p>(85) 3051-3411</p>
+          <p>{endereco}</p>
+          <p>{telefone}</p>
         </div>
       </div>
       <div className="footer-bottom">
-        <p>&copy; 2022 Digital College</p>
+        <p>&copy; {copyAno} {marca}</p>
       </div>
     </footer>
   );
 }
+
+Footer.propTypes = {
+  logoFooter: PropTypes.string.isRequired,
+  facebook: PropTypes.string,
+  instagram: PropTypes.string,
+  twitter: PropTypes.string,
+  infoLinks: PropTypes.arrayOf(PropTypes.shape({
+    href: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired
+  })).isRequired,
+  categorias: PropTypes.arrayOf(PropTypes.shape({
+    href: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired
+  })).isRequired,
+  endereco: PropTypes.string.isRequired,
+  telefone: PropTypes.string.isRequired,
+  copyAno: PropTypes.number.isRequired,
+  marca: PropTypes.string.isRequired
+};
 
 export default Footer;
